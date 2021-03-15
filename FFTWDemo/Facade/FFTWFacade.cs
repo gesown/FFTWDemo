ef5932a4517b13810bb74db014c2119293c0fd9e
Marty Grogan <owner@groganenterpriseservices.com>
2021-03-15 12:39:34 -0700

    nitial Commit

---
+++ FFTWDemo/Facade/FFTWFacade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FFTWDemo.Facade
+{
+    public static  class FFTWFacade
+    {
+        public static Entity.FFTW.ComplexArray GetOutPutArray(Entity.FFTW.DoubleArray arrayIn)
+        {
+            Entity.FFTW.DoubleArray input = arrayIn;
+            Entity.FFTW.ComplexArray output=new Entity.FFTW.ComplexArray(arrayIn.Length);
+            Entity.FFTW.ForwardTransform(input,output); 
+            return output;
+        }
+
+        public static Entity.FFTW.DoubleArray GetInputArray(string fileIn)
+        {
+            IList<string> dataIn = File.ReadAllLines(fileIn);
+            IList<double> dataInDouble = new List<double>();
+            foreach (var s in dataIn)
+            {
+                dataInDouble.Add(double.Parse(s));
+            }
+            Entity.FFTW.DoubleArray retValue =new Entity.FFTW.DoubleArray(dataInDouble.ToArray()); // not complex numbers
+            return retValue;
+        }
+    }
+}
---
+++ FFTWDemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using FFTW.NET;
+using FFTWDemo.Facade;
+using System.Linq;
+
+namespace FFTWDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileIn = args[0];
+            string fileOutR = fileIn + "_Real.txt";
+            if (File.Exists(fileOutR))
+            {
+                File.Delete(fileOutR);
+            }
+            var sr = new StreamWriter(fileOutR);
+            string fileOutI = fileIn + "_Imag.txt";
+            if (File.Exists(fileOutI))
+            {
+                File.Delete(fileOutI);
+            }
+            var si = new StreamWriter(fileOutI);
+            string fileOutA = fileIn + "_Abs.txt";
+            if (File.Exists(fileOutA))
+            {
+                File.Delete(fileOutA);
+            }
+            var sa = new StreamWriter(fileOutA);
+           var fftADIn = FFTWFacade.GetInputArray(fileIn);
+           var fftADOut = FFTWFacade.GetOutPutArray(fftADIn);
+           var fftADOR = fftADOut.Real;
+           var fftADOI = fftADOut.Imag;
+           var fftADOA = fftADOut.Abs;
+           for (int i = 0; i < fftADOR.Length; i++)
+           {
+               sr.WriteLine( fftADOR[i]);
+           }
+           for (int i = 0; i < fftADOR.Length; i++)
+           {
+               si.WriteLine(fftADOI[i]);
+           }
+           for (int i = 0; i < fftADOR.Length; i++)
+           {
+               sa.WriteLine(fftADOA[i]);
+           }
+
+           sr.Flush();
+           sr.Close();
+           si.Flush();
+           si.Close();
+           sa.Flush();
+           sa.Close();
+        }
+    }
+
+}
